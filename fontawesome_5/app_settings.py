c0fdb0eb5a228a3ee875778cced7b0a3e68ce66c
AATrubilin <aatrubilin@gmail.com>
2021-01-30 01:19:32 +0700

    change static paths

--- fontawesome_5/app_settings.py
+++ fontawesome_5/app_settings.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.templatetags.static import static
 
 
 def get_prefix():
@@ -16,11 +15,11 @@ def get_icon_renderer():
 
 
 def get_fontawesome_5_css():
-    return getattr(settings, 'FONTAWESOME_5_CSS', static('fontawesome_5/css/all.min.css'))
+    return getattr(settings, 'FONTAWESOME_5_CSS', settings.STATIC_URL + 'fontawesome_5/css/all.min.css')
 
 
 def get_css():
-    css = []
+    css = [settings.STATIC_URL + 'fontawesome_5/css/django-fontawesome.css',]
     fontawesome_5_css = get_fontawesome_5_css()
     if fontawesome_5_css:
         css.append(fontawesome_5_css)
@@ -28,7 +27,7 @@ def get_css():
 
 
 def get_css_admin():
-    css = [static('fontawesome_5/css/all.min.css')]
+    css = [settings.STATIC_URL +'fontawesome_5/css/django-fontawesome.css', settings.STATIC_URL + 'fontawesome_5/css/all.min.css']
     css_admin = getattr(settings, 'FONTAWESOME_5_CSS_ADMIN', None)
     if css_admin:
         css.append(css_admin)
